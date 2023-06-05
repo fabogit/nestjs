a5b403c2fe8cf73eb1c1da7ca60e1ef5c6616b42
fabogit <fabo.dev.email@gmail.com>
2023-06-05 12:20:35 +0200

    user entity hooks

--- 04-car-price/src/users/user.entity.ts
+++ 04-car-price/src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +17,21 @@ export class User {
 
   @Column()
   password: string;
+
+  // Hooks are executed on entityes
+  // do not push to db plain objects, make repository create entity then save/remove
+  @AfterInsert()
+  logInsert() {
+    console.log(`Inserted User with id ${this.id}`);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log(`Updated User with id ${this.id}`);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log(`Removed User with id ${this.id}`);
+  }
 }
