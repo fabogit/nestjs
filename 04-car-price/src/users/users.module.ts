36a567baaca7b66b583328464b8b8d7cfa5995c1
fabogit <fabo.dev.email@gmail.com>
2023-06-05 22:58:16 +0200

    inject user service into auth service

--- 04-car-price/src/users/users.module.ts
+++ 04-car-price/src/users/users.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { UsersController } from './users.controller';
+import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 import { User } from './user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [AuthService, UsersService],
 })
 export class UsersModule {}
