9cab64becbfbec1a373eaa460d5d154aee3b839c
fabogit <fabo.dev.email@gmail.com>
2023-06-06 00:36:50 +0200

    auth signup password hashing

--- 04-car-price/src/users/auth.service.ts
+++ 04-car-price/src/users/auth.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 
 import { UsersService } from './users.service';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    const user = await this.userService.find(email);
+    if (user.length) {
+      throw new BadRequestException('Email in use');
+    }
+    // Gen a 16chars salt
+    const salt = randomBytes(8).toString('hex');
+    //  Hash salt&pass
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    // Join hashed result and salt togheter
+    const newUserPwd = `${salt}.${hash.toString('hex')}`;
+    // Create user & save
+  }
+
+  signin() {}
 }
