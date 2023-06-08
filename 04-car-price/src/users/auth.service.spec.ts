671d484e0a6e788b9538a189b7430d0bdc5992b8
fabogit <fabo.dev.email@gmail.com>
2023-06-08 16:39:11 +0200

    more tests

--- 04-car-price/src/users/auth.service.spec.ts
+++ 04-car-price/src/users/auth.service.spec.ts
@@ -1,24 +1,53 @@
 import { Test } from '@nestjs/testing';
+import { BadRequestException } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
-it('Can create an instance of auth service', async () => {
-  const mockUsersService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
+describe('AuthService', () => {
+  let service: AuthService;
+  let mockUsersService: Partial<UsersService>;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      { provide: UsersService, useValue: mockUsersService },
-    ],
-  }).compile();
+  beforeEach(async () => {
+    mockUsersService = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const service = module.get(AuthService);
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: mockUsersService },
+      ],
+    }).compile();
 
-  expect(service).toBeDefined();
+    service = module.get(AuthService);
+  });
+
+  it('Should create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('Should create a new user with a salted and hashed password', async () => {
+    const password = 'providedpassword';
+    const user = await service.signup('user@email.com', password);
+
+    expect(user.password).not.toEqual(password);
+
+    const [salt, hash] = user.password.split('.');
+
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    mockUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+
+    await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
 });
