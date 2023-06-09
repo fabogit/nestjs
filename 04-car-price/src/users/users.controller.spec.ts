0b0e9b10ae061ac9003593622a73be5226a6c47f
fabogit <fabo.dev.email@gmail.com>
2023-06-09 16:59:37 +0200

    user controller test implementation

--- 04-car-price/src/users/users.controller.spec.ts
+++ 04-car-price/src/users/users.controller.spec.ts
@@ -1,12 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { FindRelationsNotFoundError } from 'typeorm';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let mockUserService: Partial<UsersService>;
+  let mockAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    mockAuthService = {
+      // signup: (email, password) => {},
+      // signin: (email, password) => {},
+    };
+    mockUserService = {
+      find: (email) => {
+        return Promise.resolve([
+          {
+            id: 1,
+            email,
+            password: 'pass',
+          } as User,
+        ]);
+      },
+      findOne: (id) => {
+        return Promise.resolve({
+          id,
+          email: 'mail@email.com',
+          password: 'pass',
+        } as User);
+      },
+      // remove: (id) => {},
+      // update: (id, attrs) => {},
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUserService,
+        },
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
