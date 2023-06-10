a0241d48be0483e1a7ad9d6fa3e7f3c777f6af0c
fabogit <fabo.dev.email@gmail.com>
2023-06-10 17:57:11 +0200

    POST /reports controller

--- 04-car-price/src/reports/dtos/create-report.dto.ts
+++ 04-car-price/src/reports/dtos/create-report.dto.ts
@@ -1,19 +1,37 @@
-import { IsEmail, IsString } from 'class-validator';
+import {
+  IsString,
+  IsNumber,
+  Min,
+  Max,
+  IsLatitude,
+  IsLongitude,
+} from 'class-validator';
 
 export class CreateReportDto {
-  id: number;
-
+  @IsNumber()
+  @Min(0)
+  @Max(1e6)
   price: number;
 
+  @IsString()
   maker: string;
 
+  @IsString()
   model: string;
 
+  @IsNumber()
+  @Min(1900)
+  @Max(2025)
   year: number;
 
+  @IsLatitude()
   lat: number;
 
+  @IsLongitude()
   lng: number;
 
+  @IsNumber()
+  @Min(0)
+  @Max(1e6)
   mileage: number;
 }
