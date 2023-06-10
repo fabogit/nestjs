1cd7fe85b2146c7f988b7f1f9af804b1376a5688
fabogit <fabo.dev.email@gmail.com>
2023-06-10 16:13:32 +0200

    e2e auth test setup

---
+++ 04-car-price/test/auth.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/auth/signup (POST) - handles a signup request', () => {
+    const email = 'mail@email.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'pass' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+});
