2c8781663228b4ce4a68cfbc1fdeb51b8e8e2b06
fabogit <fabo.dev.email@gmail.com>
2023-06-10 17:32:01 +0200

    adding properties to reports

--- 04-car-price/src/reports/report.entity.ts
+++ 04-car-price/src/reports/report.entity.ts
@@ -7,4 +7,22 @@ export class Report {
 
   @Column()
   price: number;
+
+  @Column()
+  maker: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  lng: number;
+
+  @Column()
+  mileage: number;
 }
